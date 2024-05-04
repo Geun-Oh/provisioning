5dd17b14929fa38b16c5a270d9c0dc86471021ef
Geun-Oh <kandy1002@naver.com>
2024-05-04 18:41:36 +0900

    feat: add gitignore var files & mod s3 backend config

--- terraform/vpc/backend.tf
+++ terraform/vpc/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-	required_version		= ">= 1.0.0"
+  required_version = ">= 1.0.0"
 
-	backend "s3" {
-		bucket				= "geun-oh-apnortheast2-tfstate"
-		key					= "terraform/vpc/geunoh_apnortheast2/terraform.tfstate"
-		region				= "ap-northeast-2"
-		encrypt				= true
-		dynamodb_table		= "terraform-lock"
-	}
+  backend "s3" {
+    bucket         = "geun-oh-apnortheast2-tfstate"
+    key            = "terraform/vpc/geunoh_apnortheast2/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
