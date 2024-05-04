fc4b573efd56787bc9049d8b307255b49d7f132a
Geun-Oh <kandy1002@naver.com>
2024-05-04 16:53:47 +0900

    feat: add global variable & route53 variable

---
+++ terraform/variables/var_route53.tf
@@ -0,0 +1,8 @@
+variable "r53_variables" {
+  default = {
+    id = {
+      star_nohv_site_acm_arn_apnortheast2 = "arn:aws:acm:ap-northeast-2:039174979026:certificate/41a527b3-3c08-4973-9c35-6ecff17426a9"
+      nohv_site_zone_id                   = "Z08229471Z5HCMP2PZOTJ"
+    }
+  }
+}
